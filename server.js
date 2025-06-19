b4ca89c7eb2a2882f38dbf756aafc0952ed1dd4a
pditi255 <ph.diethelm@hotmail.com>
2025-06-19 18:21:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-// ❗️Hier wird das aktuelle Verzeichnis als statischer Ordner verwendet
-app.use(express.static(__dirname));
-
-app.listen(port, () => {
-  console.log(`Server läuft auf Port ${port}`);
-});
-const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
